2bfc4956d77054dce69fa9f6f93b7f2c45ff1f6c
hakim <abduhakimhattalaksana@gmail.com>
2024-04-30 18:16:31 +0700

    Berubah Product

--- app/Http/Controllers/ProductImageController.php
+++ app/Http/Controllers/ProductImageController.php
@@ -8,11 +8,33 @@ use Illuminate\Http\Request;
 class ProductImageController extends Controller
 {
     public function update(Request $request) {
+        $image = $request->image;
+        $ext = $image->getClientOriginalExtension();
+        $tempImageLocation = $image->getImagePath();
+
         $productImage = new PorductImage();
-        $productImage->product_id = $product->id;
+        $productImage->product_id = $request->product_id;
         $productImage->image = 'NULL';
         $productImage->save();
 
+        $imageName = $request->product_id.'-'.$productImage->id.'.'.$ext;
+        $productImage->image = $imageName;
+        $productImage->save();
+
+        // Large Image
+        $sourcePath = $tempImageLocation;
+        $destPath = public_path() . '/uploads/product/large/' . $imageName;
+        $image = Image::make($sourcePath);
+        $image->resize(1400, null, function ($constraint) {
+            $constraint->aspectRatio();
+        });
+        $image->save($destPath);
+
+        // Small Image
+        $destPath = $tempImageLocation;
+        $image = Image::make($sourcePath);
+        $image->fit(300, 300);
+        $image->save($destPath);
         
     }
 }
